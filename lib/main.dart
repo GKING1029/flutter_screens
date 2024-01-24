0d770fae0becb4b54ca6bcd8861dddad449f19a6
Gurjot Singh <95275780+GKING1029@users.noreply.github.com>
2024-01-24 14:51:39 +0530

    uploaded new screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screen_1/screen3.dart';
+import 'package:flutter_screen_1/input_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Screen3(),
+      home: const Input_screen(),
     );
   }
 }
